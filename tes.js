a00a29cd19cc067dc6336cba368d7aead5697249
Ahmdfzii20 <fauzi200505@gmail.com>
2026-04-02 23:18:14 +0700

    create product api

--- tes.js
+++ tes.js
@@ -4,3 +4,11 @@ console.log("Hallo semua");
 
 console.log("Tes Aja");
 
+
+let nama = "Ahmad";
+let umur = 20;
+const kampus = "Potensi Utama";
+
+console.log(nama);
+console.log(umur);
+console.log(kampus);    
